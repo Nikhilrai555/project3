47e52558ac6232167439d5f35c31c2388a756b23
Nikhilrai555 <nikhilrai5555@gmail.com>
2024-08-02 00:33:13 +0530

    updates to styles

--- my-react-app/src/home1Assets/Navbar.jsx
+++ my-react-app/src/home1Assets/Navbar.jsx
@@ -3,11 +3,17 @@ const Navbar = () => {
     const navigate = useNavigate()
     const user =null
   return (
-   <div className="bg-gray-950 text-white ">
-    <nav ><button className="bg-gray-950 text-white" onClick={()=>{navigate('/auth')}}>Get Started</button></nav>
-        <h1 className="text-xl font-bold text-red-500" >Welcome to our application</h1>
-        <button onClick={()=>{navigate('/auth')}
-        }>Get Started</button>
+   <div className="bg-slate-950 h-screen">
+    <nav className="flex items-center justify-between p-6 lg:px-8">
+      <h2 className="text-white font-bold text-xl">CryptoCalls</h2>
+      <button className=" text-white" onClick={()=>{navigate('/auth')}}>Get Started</button>
+      </nav>
+      <div className="flex lg:flex-1 items-center justify-around h-full" > 
+        <h1 className="text-xl font-bold text-white" >Welcome to our application</h1>
+        <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quia?</p>
+      
+        </div>
+       
    </div>
   )
 }
